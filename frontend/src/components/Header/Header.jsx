91115e298e48cc152e325816ac6790691fa86ed4
mehrin <mehrinpp0@gmail.com>
2025-03-18 22:27:05 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+          <h2>Order Your Favourite Food </h2>
+          <p>Satisfy your cravings in style Hungry? Order now to get your food delivered in less than 30 minutes – guaranteed.</p>
+          <button>View Menu</button>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
